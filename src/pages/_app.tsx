84a419fa8f09f94b75c56e3258558258f84d2962
Guilherme Illescas <gui.illescas@gmail.com>
2021-04-29 23:32:11 -0300

    Add configs and components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,22 @@
-import { ReactElement } from "react";
+import 'react-toastify/dist/ReactToastify.css';
 
-function MyApp({ Component, pageProps }): ReactElement {
-  return <Component {...pageProps} />
-};
+import { ReactElement } from 'react';
+import { AppProps } from 'next/dist/next-server/lib/router/router';
+import { ToastContainer } from 'react-toastify';
+import Modal from 'react-modal';
+
+import { GlobalStyles } from '../styles/globals';
+
+Modal.setAppElement('#__next');
+
+function MyApp({ Component, pageProps }: AppProps): ReactElement {
+  return (
+    <>
+      <GlobalStyles />
+      <ToastContainer autoClose={4000} />
+      <Component {...pageProps} />
+    </>
+  );
+}
 
 export default MyApp;
