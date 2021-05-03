9358b5c93c93ab225ca03449af49178ce5a7c13c
Guilherme Illescas <gui.illescas@gmail.com>
2021-05-02 22:33:49 -0300

    Create burger menu

---
+++ src/components/MenuOfBurger/styles.ts
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+interface IStyledMenuProps {
+  open: boolean;
+}
+
+export const StyledMenu = styled.nav<IStyledMenuProps>`
+  position: absolute;
+  display: ${open => (open ? 'flex' : 'none')};
+  flex-direction: column;
+  justify-content: center;
+  background: #effffa;
+  transform: ${open => (open ? 'translateX(0)' : 'translateX(-100%)')};
+  height: 100vh;
+  text-align: left;
+  padding: 2rem;
+  position: absolute;
+  top: 0;
+  right: 0;
+  transition: transform 0.3s ease-in-out;
+
+  z-index: 9;
+
+  @media (max-width: 576px) {
+    width: 100%;
+  }
+
+  a {
+    font-size: 2rem;
+    text-transform: uppercase;
+    padding: 2rem 0;
+    font-weight: bold;
+    letter-spacing: 0.5rem;
+    color: #0d0c1d;
+    text-decoration: none;
+    transition: color 0.3s linear;
+
+    @media (max-width: 576px) {
+      font-size: 1.5rem;
+      text-align: center;
+    }
+
+    &:hover {
+      color: #343078;
+    }
+  }
+`;
