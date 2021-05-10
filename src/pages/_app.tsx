4ae0be951d00d6022b030e092e3457445f4fff6e
Guilherme Illescas <gui.illescas@gmail.com>
2021-05-09 23:50:39 -0300

    Insert global app hooks provider'

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,17 +5,19 @@ import { AppProps } from 'next/dist/next-server/lib/router/router';
 import { ToastContainer } from 'react-toastify';
 import Modal from 'react-modal';
 
+import AppProvider from '../hooks';
+
 import { GlobalStyles } from '../styles/globals';
 
 Modal.setAppElement('#__next');
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
   return (
-    <>
+    <AppProvider>
       <GlobalStyles />
       <ToastContainer autoClose={4000} />
       <Component {...pageProps} />
-    </>
+    </AppProvider>
   );
 }
 
