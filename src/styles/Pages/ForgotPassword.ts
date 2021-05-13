508be113a9d9557ed7713d65da5ff53f15fdfbc0
Guilherme Illescas <gui.illescas@gmail.com>
2021-05-13 13:43:08 -0300

    Finish page styles

---
+++ src/styles/Pages/ForgotPassword.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const StylesContainer = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+
+  form {
+    max-width: 768px;
+    margin: 0 auto;
+
+    margin-top: 4rem;
+  }
+`;
