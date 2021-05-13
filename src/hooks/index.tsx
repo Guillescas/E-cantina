9f24878486e6a7d293cd825c44712ff613e8239b
Guilherme Illescas <gui.illescas@gmail.com>
2021-05-13 13:43:39 -0300

    Create auth hook

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -2,9 +2,9 @@ import { BurgerProvider } from './burger';
 import { SignInModalContextProvider } from './signinModal';
 
 const AppProvider: React.FC = ({ children }: any) => (
-  <SignInModalContextProvider>
-    <BurgerProvider>{children}</BurgerProvider>
-  </SignInModalContextProvider>
+  <BurgerProvider>
+    <SignInModalContextProvider>{children}</SignInModalContextProvider>
+  </BurgerProvider>
 );
 
 export default AppProvider;
