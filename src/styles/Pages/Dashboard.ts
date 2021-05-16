4fee0cda9b889a993a0a4eec6edba2696fc5a30a
Guilherme Illescas <gui.illescas@gmail.com>
2021-05-15 22:06:26 -0300

    Increase dashboard styles

--- src/styles/Pages/Dashboard.ts
+++ src/styles/Pages/Dashboard.ts
@@ -1,3 +1,27 @@
 import styled from 'styled-components';
 
 export const StylesContainer = styled.div``;
+
+export const Content = styled.div`
+  max-width: 1300px;
+
+  margin: 2rem auto;
+
+  display: flex;
+  align-items: top;
+  justify-content: space-between;
+
+  position: relative;
+`;
+
+export const ContentList = styled.div`
+  background: var(--second-background);
+  border-radius: 0.75rem;
+
+  width: 100%;
+  margin-left: 1rem;
+
+  min-height: 1200px;
+
+  padding: 1rem;
+`;
