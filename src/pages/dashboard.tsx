b6d9d7fc1ad84761a7e14f6275aee5b252b863c5
Guilherme Illescas <gui.illescas@gmail.com>
2021-05-15 22:06:16 -0300

    Add components on dashboard page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,12 +1,27 @@
 import { ReactElement } from 'react';
+
+import LeftDashboardMenu from '../components/LeftDashboardMenu';
+import RestaurantCard from '../components/RestaurantCard';
 import TopDashboardMenu from '../components/TopDashboardMenu';
 
-import { StylesContainer } from '../styles/Pages/Dashboard';
+import {
+  StylesContainer,
+  Content,
+  ContentList,
+} from '../styles/Pages/Dashboard';
 
 const Dashboard = (): ReactElement => {
   return (
     <StylesContainer>
       <TopDashboardMenu />
+
+      <Content>
+        <LeftDashboardMenu />
+
+        <ContentList>
+          <RestaurantCard />
+        </ContentList>
+      </Content>
     </StylesContainer>
   );
 };
