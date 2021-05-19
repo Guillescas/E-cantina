f247392492a41b90b470c971bd01319b47b1b116
Guilherme Illescas <gui.illescas@gmail.com>
2021-05-19 13:03:11 -0300

    Get infos from API

--- src/components/RestaurantCard/index.tsx
+++ src/components/RestaurantCard/index.tsx
@@ -1,30 +1,35 @@
+import Link from 'next/link';
 import { ReactElement } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
 import { StylesContainer } from './styles';
 
 interface IRestaurantCardProps {
+  id: number;
   name: string;
   description: string;
 }
 
 const RestaurantCard = ({
+  id,
   name,
   description,
 }: IRestaurantCardProps): ReactElement => {
   return (
-    <StylesContainer>
-      <img src="/assets/restaurant.jpeg" alt={`Imagem de ${name}`} />
+    <Link href={`/restaurant/${id}`}>
+      <StylesContainer>
+        <img src="/assets/restaurant.jpeg" alt={`Imagem de ${name}`} />
 
-      <div className="infos">
-        <h2>{name}</h2>
-        <p>{description}</p>
-      </div>
+        <div className="infos">
+          <h2>{name}</h2>
+          <p>{description}</p>
+        </div>
 
-      <div className="icon">
-        <FiChevronRight size={22} />
-      </div>
-    </StylesContainer>
+        <div className="icon">
+          <FiChevronRight size={22} />
+        </div>
+      </StylesContainer>
+    </Link>
   );
 };
 
