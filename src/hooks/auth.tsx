d66ed10e3fc67dba284ba91c50960551383d7f4b
Guilherme Illescas <gui.illescas@gmail.com>
2021-05-19 10:16:00 -0300

    Add new functions and variables on auth context

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -3,6 +3,8 @@
 import React, { createContext, useCallback, useState, useContext } from 'react';
 import jwt_decode from 'jwt-decode';
 import Cookie from 'js-cookie';
+import { useRouter } from 'next/router';
+import { toast } from 'react-toastify';
 
 import { IClientProps } from '../@types';
 
@@ -18,12 +20,6 @@ interface SignInCredentials {
   password: string;
 }
 
-interface AuthContextData {
-  user: IClientProps;
-  signIn(credentials: SignInCredentials): Promise<void>;
-  signOut(): void;
-}
-
 interface ITokenResponse {
   sub: string;
   email: string;
@@ -33,9 +29,18 @@ interface ITokenResponse {
   exp: number;
 }
 
+interface AuthContextData {
+  token: string;
+  user: IClientProps;
+  signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
+}
+
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({ children }: any) => {
+  const router = useRouter();
+
   const [data, setData] = useState<AuthState>(() => {
     const token = Cookie.get('@ECantina:token');
     const user = Cookie.get('@ECantina:user');
@@ -47,43 +52,56 @@ export const AuthProvider: React.FC = ({ children }: any) => {
     return {} as AuthState;
   });
 
-  const signIn = useCallback(async ({ email, password }) => {
-    await api
-      .post('/login', {
+  const signIn = useCallback(
+    async ({ email, password }) => {
+      const userData = {
         email,
         password,
-      })
-      .then(response => {
-        const { token } = response.data;
+      };
+      api
+        .post('/login', userData)
+        .then(response => {
+          const { token } = response.data;
 
-        const decodedJWTToken: ITokenResponse = jwt_decode(token);
+          const decodedJWTToken: ITokenResponse = jwt_decode(token);
 
-        const formattedUserInfosFromToken = {
-          sub: decodedJWTToken.sub,
-          email: decodedJWTToken.email,
-          name: decodedJWTToken.name,
-          type: decodedJWTToken.type,
-        };
+          const formattedUserInfosFromToken = {
+            sub: decodedJWTToken.sub,
+            email: decodedJWTToken.email,
+            name: decodedJWTToken.name,
+            type: decodedJWTToken.type,
+          };
 
-        Cookie.set('@ECantina:token', token);
-        Cookie.set(
-          '@ECantina:user',
-          JSON.stringify(formattedUserInfosFromToken),
-        );
+          Cookie.set('@ECantina:token', token);
+          Cookie.set(
+            '@ECantina:user',
+            JSON.stringify(formattedUserInfosFromToken),
+          );
 
-        setData({ token, user: formattedUserInfosFromToken });
-      });
-  }, []);
+          setData({ token, user: formattedUserInfosFromToken });
 
-  const signOut = useCallback(() => {
+          router.push('/dashboard');
+        })
+        .catch(error => {
+          return toast.error('Ocorreu um erro ao realizar login');
+        });
+    },
+    [router],
+  );
+
+  const signOut = useCallback(async () => {
+    await router.push('/');
+
     Cookie.remove('@ECantina:token');
     Cookie.remove('@ECantina:user');
 
     setData({} as AuthState);
-  }, []);
+  }, [router]);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider
+      value={{ token: data.token, user: data.user, signIn, signOut }}
+    >
       {children}
     </AuthContext.Provider>
   );
