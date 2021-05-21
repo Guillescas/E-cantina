d8b81a616143707cfb13621697448fb229744d67
Guilherme Illescas <gui.illescas@gmail.com>
2021-05-21 12:10:57 -0300

    Intercept errors of 403 code

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,7 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+import Cookie from 'js-cookie';
+import Router from 'next/router';
+import { toast } from 'react-toastify';
 
 const api = axios.create({
   baseURL: 'http://localhost:8080',
@@ -6,5 +9,23 @@ const api = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+api.interceptors.response.use(
+  response => {
+    return response;
+  },
+  (error: AxiosError) => {
+    if (error.response.status === 403) {
+      Cookie.remove('@ECantina:token');
+      Cookie.remove('@ECantina:user');
+
+      Router.push('/');
+
+      toast.info('Seu token foi expirado. Faça login novamente');
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export default api;
