954ef9622dfda2e1391a6a596dd8cade7a0d687a
Guilherme Illescas <gui.illescas@gmail.com>
2021-05-23 01:25:48 -0300

    Configure authentication and add axios interceptor

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,31 +1,37 @@
-import axios, { AxiosError } from 'axios';
+import axios, { AxiosError, AxiosInstance } from 'axios';
 import Cookie from 'js-cookie';
 import Router from 'next/router';
+import { parseCookies } from 'nookies';
 import { toast } from 'react-toastify';
 
-const api = axios.create({
-  baseURL: 'http://localhost:8080',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
+export function setupAPIClient(ctx = undefined): AxiosInstance {
+  const cookies = parseCookies(ctx);
 
-api.interceptors.response.use(
-  response => {
-    return response;
-  },
-  (error: AxiosError) => {
-    if (error.response.status === 403) {
-      Cookie.remove('@ECantina:token');
-      Cookie.remove('@ECantina:user');
+  const api = axios.create({
+    baseURL: 'http://localhost:8080',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${cookies['@ECantina:token']}`,
+    },
+  });
 
-      Router.push('/');
+  api.interceptors.response.use(
+    response => {
+      return response;
+    },
+    (error: AxiosError) => {
+      if (error.response.status === 403) {
+        Cookie.remove('@ECantina:token');
+        Cookie.remove('@ECantina:user');
 
-      toast.info('Seu token foi expirado. Faça login novamente');
-    }
+        Router.push('/');
 
-    return Promise.reject(error);
-  },
-);
+        toast.info('Seu token foi expirado. Faça login novamente');
+      }
 
-export default api;
+      return Promise.reject(error);
+    },
+  );
+
+  return api;
+}
