1a7b09227449b5e38b6924fd6f8660b5de1a7ee7
Kenzo De Albuquerque <64553441+OzneKx@users.noreply.github.com>
2021-06-02 22:29:05 -0300

    NotFound branch criada

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound:React.FC = () => {
+  return (
+    <StylesContainer>
+      <img src="" alt="notFound" />
+    </StylesContainer>
+  );
+}
+
+export default NotFound;
