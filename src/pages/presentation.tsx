6fac264d3e891527a410debbaa4eae3db501518d
Kenzo De Albuquerque <64553441+OzneKx@users.noreply.github.com>
2021-06-02 22:12:07 -0300

    Finalizada a presentation

--- src/pages/presentation.tsx
+++ src/pages/presentation.tsx
@@ -1,14 +1,20 @@
+import { useRouter } from 'next/router';
 import { ReactElement } from 'react';
 import { StylesContainer } from '../styles/Pages/Presentation';
 
 const Presentation = (): ReactElement => {
+  const router = useRouter();
+
   return (
     <StylesContainer>
       <div className="images">
         <img src="/assets/presentation.svg" alt="Food" />
 
         <img src="/assets/logo.png" alt="Logo" />
-        <button type="button">CONHEÇA JÁ</button>
+
+        <button type="button" onClick={() => router.push('/')}>
+          CONHEÇA JÁ
+        </button>
       </div>
 
       <div className="text">
