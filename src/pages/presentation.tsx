acd08b747e8a2bfa5a80a442cb05356562703818
Kenzo De Albuquerque <64553441+OzneKx@users.noreply.github.com>
2021-06-02 21:58:41 -0300

    Estilização da Presentation

--- src/pages/presentation.tsx
+++ src/pages/presentation.tsx
@@ -1,10 +1,19 @@
-import { ReactElement, useState } from 'react';
+import { ReactElement } from 'react';
 import { StylesContainer } from '../styles/Pages/Presentation';
 
 const Presentation = (): ReactElement => {
   return (
     <StylesContainer>
-      <img src="/assets/logo.png" alt="Logo" />
+      <div className="images">
+        <img src="/assets/presentation.svg" alt="Food" />
+
+        <img src="/assets/logo.png" alt="Logo" />
+        <button type="button">CONHEÇA JÁ</button>
+      </div>
+
+      <div className="text">
+        <p>Uma aplicação desenvolvida para resolver um problema</p>
+      </div>
     </StylesContainer>
   );
 };
