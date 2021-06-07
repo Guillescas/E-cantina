1349ec029f0dce23fdeea4454e73087d6bc58a79
Guilherme Illescas <gui.illescas@gmail.com>
2021-06-07 09:09:16 -0300

    Add disabled property

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -8,14 +8,16 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
 interface IButtonProps extends ButtonProps {
   isLoading?: boolean;
+  isDisabled?: boolean;
 }
 
 const Button = ({
   children,
   isLoading,
+  isDisabled,
   ...rest
 }: IButtonProps): ReactElement => (
-  <StylesContainer type="button" {...rest} disabled={isLoading}>
+  <StylesContainer type="button" {...rest} disabled={isLoading || isDisabled}>
     {isLoading ? <Loading /> : children}
   </StylesContainer>
 );
