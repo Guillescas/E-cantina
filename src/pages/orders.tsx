221b629638f6089e7d086339012e12ef9c8503b9
Guilherme Illescas <gui.illescas@gmail.com>
2021-06-07 09:11:35 -0300

    Finish order page

--- src/pages/orders.tsx
+++ src/pages/orders.tsx
@@ -1,13 +1,93 @@
-import { ReactElement } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
 
 import BaseDashboard from '../components/BaseDashboard';
+import OrderCard from '../components/OrderCard';
+import { useAuth } from '../hooks/auth';
+
+import { api } from '../services/apiClient';
 
 import { StylesContainer } from '../styles/Pages/Orders';
 
+interface IProductProps {
+  id: number;
+  name: string;
+  type: string;
+  description: string;
+  price: number;
+  urlImage: string;
+}
+
+interface IProductListProps {
+  id: number;
+  quantity: number;
+  value: number;
+  total: number;
+  description: string;
+  product: IProductProps;
+}
+
+interface IOrderProps {
+  id: number;
+  finished: boolean;
+  createdAt: string;
+  finishAt: string;
+  observation: string;
+  valid: boolean;
+  total: number;
+  productLists: IProductListProps[];
+}
+
 const Orders = (): ReactElement => {
+  const { user } = useAuth();
+
+  const [orders, setOrders] = useState<IOrderProps[]>([]);
+
+  useEffect(() => {
+    api.get(`/client/${user.sub}`).then(response => {
+      console.log(response.data.orders);
+      setOrders(response.data.orders);
+    });
+  }, [user]);
+
   return (
     <BaseDashboard>
-      <h1>Meus pedidos</h1>
+      <StylesContainer>
+        <h1>Meus pedidos</h1>
+        <div className="orders">
+          {orders.map(order => {
+            if (!order.finished) {
+              return (
+                <OrderCard
+                  key={order.id}
+                  id={order.id}
+                  orderAt={order.createdAt}
+                  totalPrice={order.total}
+                  productList={order.productLists}
+                />
+              );
+            }
+            return '';
+          })}
+        </div>
+
+        <h2>Histórico</h2>
+        <div className="orders">
+          {orders.map(order => {
+            if (order.finished) {
+              return (
+                <OrderCard
+                  key={order.id}
+                  id={order.id}
+                  orderAt={order.createdAt}
+                  totalPrice={order.total}
+                  productList={order.productLists}
+                />
+              );
+            }
+            return '';
+          })}
+        </div>
+      </StylesContainer>
     </BaseDashboard>
   );
 };
